0ddc42c8f9a8b056587f60c4abeb3d72e3734dbe
bibhu <bibhu.phy@gmail.com>
2015-10-29 16:07:57 -0500

    Estimation codes

---
+++ test/copy.sh
@@ -0,0 +1,6 @@
+scp ./*.png bmahakud@lxplus.cern.ch:/afs/cern.ch/user/b/bmahakud/www/RA2bSUSY/ZinvEstimate/ProductionV3/DataMCPlotsAndOtherPlots
+
+
+
+
+scp ./*.gif bmahakud@lxplus.cern.ch:/afs/cern.ch/user/b/bmahakud/www/RA2bSUSY/ZinvEstimate/ProductionV3/DataMCPlotsAndOtherPlots
